b2e9a96ae53949eaec5879f47e97dd29893eb8b2
Faraoman <faraoman@live.it>
2021-08-08 23:10:13 +0200

    Update touchpad-enable.sh

--- touchpad-enable.sh
+++ touchpad-enable.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 qdbus-qt5 org.kde.kded5 /modules/touchpad org.kde.touchpad.enable
+qdbus-qt5 org.freedesktop.Notifications /org/kde/osdService org.kde.osdService.touchpadEnabledChanged true
